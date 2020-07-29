c4fa72a70e55d69f86b59d85ff5ad0843edf2664
maxtrewartha <maxtrewartha@gmail.com>
2020-07-29 22:24:00 +0100

    changed validation and removed requirement of usernames

--- models/User.js
+++ models/User.js
@@ -2,21 +2,11 @@ const mongoose = require("mongoose");
 const addressSchema = require("./Address");
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        min: 3,
-        max: 255
-    },
     email: {
         type: String,
         required: true,
         min: 6,
         max: 255
-    },
-    organizations: {
-        type: Array,
-        required: false
     },
     password: {
         type: String,
