1e1c624ab79bed2f7acec5fef005c5f1906160ee
maxtrewartha <maxtrewartha@gmail.com>
2020-07-31 01:01:56 +0100

    made qrs workable

--- routes/qr.js
+++ routes/qr.js
@@ -5,7 +5,7 @@ router.get("/qr-code", async (req, res) => {
 
     var qrs = [];
     for (i = 0; i < 15; i++) {
-        qrs.push(uuidv4());
+        qrs.push("deliverly" + uuidv4().split("-").join(""));
     }
     res.send({
         "success": true,
