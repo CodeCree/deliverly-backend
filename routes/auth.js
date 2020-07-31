40848bebb197c642985920753e662fa741a7a4a5
maxtrewartha <maxtrewartha@gmail.com>
2020-07-31 21:06:19 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -78,9 +78,9 @@ router.put("/:id", verify, verifyOp, async (req, res) => {
             "error": error.details[0].message
         });
     }
-    
+
     //Check email
-    const emailExists = await UserModel.findOne({ email: req.body.email, _id: { $ne: user._id }});
+    const emailExists = await UserModel.findOne({ email: req.body.email, _id: { $ne: user._id } });
     if (emailExists) return res.status(400).send({ "success": false, "error": "Email has already been registered" });
 
     //Check op if same user
@@ -144,6 +144,75 @@ router.get("/me", verify, async (req, res) => {
         "data": formatUserJson(req.user)
     });
 });
+
+router.put("/me", verify, async (req, res) => {
+
+    // Need to get the user id from jwt token
+    try {
+        // trys to verify token
+        var verified = jwt.verify(req.header("Authorization"), process.env.TOKEN_SECRET);
+        // finds the user from the token
+        var user = await userModel.find({ _id: verified._id })
+        // If the user exists, continue
+        if (user) continue;
+    } catch (e) {
+        // If jwt cant verify token
+        res.status(400).send({ "success": false, "error": "Invalid token" });
+    }
+
+
+    //Check user exists
+    var user = await UserModel.findOne({ _id: req.params.id });
+    if (!user) return res.status(400).send({
+        "success": false,
+        "error": 'User does not exist'
+    });
+
+    //Validation
+    if (req.body.password) {
+        const { error } = userPasswordValidation(req.body);
+        if (error) return res.status(400).send({
+            "success": false,
+            "error": error.details[0].message
+        });
+    }
+    else {
+        const { error } = userValidation(req.body);
+        if (error) return res.status(400).send({
+            "success": false,
+            "error": error.details[0].message
+        });
+    }
+
+    //Check email
+    const emailExists = await UserModel.findOne({ email: req.body.email, _id: { $ne: user._id } });
+    if (emailExists) return res.status(400).send({ "success": false, "error": "Email has already been registered" });
+
+    //Check op if same user
+    //if (req.user._id === user._id && !req.body.operator) return res.status(400).send({ "success": false, "error": "You cannot demote yourself" });
+
+    //Password validation
+    if (req.body.password) {
+        const hashedPassword = await bcrypt.hash(req.body.password, await bcrypt.genSalt(10));
+        user.password = hashedPassword;
+    }
+
+    user.firstName = req.body.firstName;
+    user.lastName = req.body.lastName;
+    user.email = req.body.email;
+
+    // Save + catch error
+    try {
+        await user.save();
+        var users = await UserModel.find().sort({ email: 1 });
+        res.send({ "success": true, "data": users.map(user => formatUserJson(user)) });
+    } catch (error) {
+        res.status(400).send(error);
+    }
+
+
+});
+
 
 router.get("", verify, verifyOp, async (req, res) => {
     var users = await UserModel.find().sort({ email: 1 });
